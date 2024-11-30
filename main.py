a06b8c6056b1ee884d19c29dc19458076b5f837c
Henry Tsai <henry88002605@gmail.com>
2024-11-30 21:07:59 +1000

    using game loop by pygame lib

--- main.py
+++ main.py
@@ -1,13 +1,27 @@
+from constants import *
 import pygame
-
-import constants
 
 
 def main():
-    print("Starting asteroids!")
-    print(f"Screen width: {constants.SCREEN_WIDTH}")
-    print(f"Screen height: {constants.SCREEN_HEIGHT}")
+    # Initialize
+    pygame.init()
+
+    # Create a display window
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    pygame.display.set_caption("Asteroids Game")
+
+    # Main game loop
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        # Use the screen's fill method to fill the screen with a solid "black" color
+        screen.fill((0, 0, 0))
+
+        # Use pygame's display.flip() method to refresh the screen. Be sure to call this last!
+        pygame.display.flip()
 
 
 if __name__ == "__main__":
+    print("Starting asteroids!")
     main()
