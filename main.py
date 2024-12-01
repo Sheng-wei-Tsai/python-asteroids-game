33f6587175553772563c311edbb2067d1311f2ed
Henry Tsai <henry88002605@gmail.com>
2024-12-01 20:30:13 +1000

    add fps

--- main.py
+++ main.py
@@ -1,23 +1,29 @@
-from constants import *
 import pygame
+from constants import *
 
 
 def main():
-    # Initialize
     pygame.init()
 
     # Create a display window
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     pygame.display.set_caption("Asteroids Game")
 
+    clock = pygame.time.Clock()
+    dt = 0
+
     # Main game loop
     while True:
         for event in pygame.event.get():
+
             if event.type == pygame.QUIT:
                 return
 
         screen.fill("Black")
         pygame.display.flip()
+
+        # cap the frame
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
