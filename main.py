3d5fee9072545c374e4d96df8f77d3f29655ebd8
SHENG-WEI TSAI <henry88002605@gmail.com>
2026-02-02 19:45:59 +1000

    finish the logging function and finialise the project

--- main.py
+++ main.py
@@ -4,34 +4,40 @@ from player import Player
 from asteroid import Asteroid
 from asteroidField import AsteroidField
 from shot import Shot
+
 def main():
     pygame.init()
+
+    print("Starting Asteroids")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+
 
     # Create a display window
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     pygame.display.set_caption("Asteroids Game")
-    
+
     clock = pygame.time.Clock()
     dt = 0
-    
+
     #create groups
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
     shots = pygame.sprite.Group()
-    
-    
+
+
     # Initialize the player and add it to groups
     Shot.containers = (shots, updatable, drawable)
     Asteroid.containers = (asteroids, updatable, drawable)
     AsteroidField.containers = (updatable)
     Player.containers = (updatable, drawable)
-    
-    # initialise the player and asteroid filed 
+
+    # initialise the player and asteroid filed
     asteroid_field = AsteroidField()
-    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2) 
-    
-    
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+
     # Main game loop
     while True:
         for event in pygame.event.get():
@@ -40,21 +46,21 @@ def main():
         # update all objects in the updatable group
         for obj in updatable:
             obj.update(dt)
-            
+
         # Collision detection between player and asteroids
         for asteroid in asteroids:
             if player.check_collision(asteroid):
                 print("Game Over")
-                return pygame.quit()        
+                return pygame.quit()
         # Collision detection between bullets and asteroids
             for shot in shots:
                 if shot.check_collision(asteroid):
                     asteroid.split()
                     shot.kill()
-        
-        # Draw all objects in the drawable group        
+
+        # Draw all objects in the drawable group
         screen.fill("Black")
-        
+
         for obj in drawable:
             obj.draw(screen)
 
